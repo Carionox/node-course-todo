c93880c5784ec97abe65ba9497495b8f3a5a7e13
NV <varma.narayan@gmail.com>
2018-10-10 14:06:13 +0530

    Added Get todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,19 @@ app.post('/todos',(req, response)=>{
     
 });
 
+app.get('/todos',(req, res)=>{
+    Todo.find({}).then((todos)=>{
+
+        res.send({todos});
+
+    }, (e)=>{
+
+        res.status(400).send(e);
+
+    });
+
+
+});
 var createTodo = (taskText, res)=>{
 
     var newTodo = new Todo({
