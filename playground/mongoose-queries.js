32e05be90b9c73cdd3acf082d1d02a69943e4547
NV <varma.narayan@gmail.com>
2018-10-11 20:13:18 +0530

    Added Mongoose Queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,56 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('../server/db/mongoose');
+const {Todo} = require('../server/models/todo');
+const {User} = require('../server/models/user'); 
+
+// var _id = '5bbdbddd64574f3ac8f95600';
+var _id = '5bb95b9bd803ee23285a860c';
+
+// if(!ObjectID.isValid(_id)){
+
+//     console.log('ID specified is INVALID');
+
+// }
+
+// Todo.find({
+
+//     _id: _id
+
+// }).then((todos)=>{
+
+//     console.log('Todos', todos);
+
+// });
+
+// Todo.findOne({
+
+//     _id: _id
+
+// }).then((todo)=>{
+
+//     console.log('Todo', todo);
+
+// });
+
+// Todo.findById(_id).then((todo)=>{
+//     if(!todo){
+
+//         return console.log('ID not found');
+
+//     }
+//     console.log('Todo By ID', todo);
+
+// }).catch((e)=>{console.log(e)});
+
+User.findById(_id).then((user)=>{
+
+    if(!user){
+
+        return console.log('No such user EXISTS');
+
+    }
+
+    console.log('User Found', JSON.stringify(user));
+
+}, (e)=>{console.log(e)});
