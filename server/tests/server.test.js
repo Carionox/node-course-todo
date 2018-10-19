e06645b55534324b837514c2bf7f27cf2110320d
NV <varma.narayan@gmail.com>
2018-10-19 19:25:53 +0530

    Added test cases for GET/todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,11 @@
 const expect = require('expect')
 const request = require('supertest');
-
+const {ObjectID} = require ('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-const todos = [{text:'First test todo'},
-                {text:'Second test todo'}]
+const todos = [{_id: new ObjectID(), text:'First test todo'},
+                {_id: new ObjectID(), text:'Second test todo'}]
 
 
 beforeEach((done)=>{
@@ -72,4 +72,40 @@ describe('GET /todos',()=>{
         })
         .end(done);
     });
+});
+
+describe('GET /todos/:id', ()=>{
+
+    it('should return todo doc',(done)=>{
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+
+    });
+
+});
+
+// it should return a 404 if todo not found 
+describe('GET /todos/:id',()=>{
+    it('should return a 404 if _id not found',(done)=>{
+        request(app)
+        .get(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done);
+    })
+});
+
+
+// it should return a 404 for non object ID
+describe('GET /todos/:id',()=>{
+    it('should return a 404 if _id invalid',(done)=>{
+        request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done);
+    })
 });
